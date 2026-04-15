5f3ab164f9fc4709339c05934a3716e653aa3ea3
Diana Bali <dianabali64@gmail.com>
2026-04-15 09:05:25 +0200

    ex00

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
+
+/* int main(void)
+{
+    int number;
+
+    ft_ft(&number);
+    printf("The value of number is: %d\n", number);
+    return 0;
+} */
---
+++ C01/ex00/subject.md
@@ -0,0 +1,7 @@
+# Subject
+Create a function that takes a pointer to an int as a parameter and sets the value of that int to "42".
+
+The function should be prototyped as follows:
+```
+void ft_ft(int *nbr);
+```

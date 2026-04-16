5d76f5c36e360e3be9d1ab0d9fd4fb3679da1360
Diana Bali <dianabali64@gmail.com>
2026-04-16 14:32:03 +0200

    ex02

---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/* int	main(void)
+{
+	int a = 10;
+	int b = 20;
+	
+	printf("Before: \n");
+	printf("a: %d\n", a);
+	printf("b: %d\n", b);
+	
+	ft_swap(&a, &b);
+	
+	printf("After: \n");
+	printf("a: %d\n", a);
+	printf("b: %d\n", b);
+} */

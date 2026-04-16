383a29ef6893aae3d62887f8c45e84594b012726
Diana Bali <dianabali64@gmail.com>
2026-04-16 14:34:41 +0200

    ex03

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+/* int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 10;
+	b = 2;
+	ft_div_mod(a, b, &div, &mod);
+	printf("div: %d, mod: %d", div, mod);
+	return (0);
+} */

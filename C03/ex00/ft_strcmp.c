6fae7519048e0083877cb506c4683685b84f437b
Diana Bali <dianabali64@gmail.com>
2026-04-18 10:01:56 +0200

    ex00

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -1,8 +1,8 @@
 // #include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
@@ -10,7 +10,7 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-/* int	main(void)
+/* int main(void)
 {
 	char *s1 = "12";
 	char *s2 = "12";

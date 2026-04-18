2ec99b50acf57ba47106fcc3d5c1b2675c246dae
Diana Bali <dianabali64@gmail.com>
2026-04-18 10:12:51 +0200

    ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+
+char *ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (to_find == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j])
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return (0);
+}
+
+/* int main(void)
+{
+	char str[] = "aab";
+	char to_find_1[] = "ab";
+	char to_find_2[] = "yyyy";
+	char to_find_3[] = "";
+
+	printf("FOUND: %s\n", ft_strstr(str, to_find_1));
+	printf("NOT FOUND: %s\n", ft_strstr(str, to_find_2));
+	printf("EMPTY: %s", ft_strstr(str, to_find_3));
+} */

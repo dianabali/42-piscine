f7db43e5429c44723ae14cf1dc0b9ff5aae03d2b
Diana Bali <dianabali64@gmail.com>
2026-04-19 14:17:54 +0200

    ex00

---
+++ C04/ft_strlen.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/* int main(void)
+{
+	char str[] = "12345";
+	printf("%d", ft_strlen(str));
+	return (0);
+} */
